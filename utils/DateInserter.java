481534b72a68d9062ee07589fcceac636dff1343
git <82054121+KXzeno@users.noreply.github.com>
2024-12-01 14:07:08 -0800

    FEAT: implement Java file parser and updater
    This is planned to automate the Date edit and create fields for each record compilation.

--- utils/DateInserter.java
+++ utils/DateInserter.java
@@ -1,6 +1,16 @@
 package utils;
 
-// import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+// import java.nio.file.FileSystem;
+// import java.nio.file.StandardOpenOption;
+import java.lang.CharSequence;
+
+import java.nio.file.Path;
 
 public class DateInserter {
   public static void main(String[] args) {
@@ -11,7 +21,30 @@ public class DateInserter {
         input.append(arg).append(" ");
       }
       // TODO: Begin file search
-      // Start at likely paths (e.g., ./app/)
+      String inputPath = input.toString().trim();
+      File inputFile = new File(new StringBuilder("./").append(inputPath).toString());
+      Path target = inputFile.toPath();
+      if (inputFile.exists() && inputFile.canRead() && inputFile.canWrite()) {
+        try {
+          BufferedReader reader = Files.newBufferedReader(target, StandardCharsets.UTF_8);
+          CharSequence csq = reader.lines().map(line -> {
+            if (line.contains("C Date")) {
+              return "{Pending Creation Date}";
+            } else if (line.contains("E Date")) {
+              return "{Pending Edited Date}";
+            } else {
+              return line;
+            }
+          }).reduce("", (a, b) -> a + b + "\n");
+
+          reader.close();
+          BufferedWriter writer = Files.newBufferedWriter(target, StandardCharsets.UTF_8);
+          writer.append(csq);
+          writer.close();
+        } catch (IOException x) {
+          x.printStackTrace();
+        }
+      }
     } else {
       System.out.println("No args");
     }
