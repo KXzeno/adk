1e0b2eaa6b59921417ead4a610a34ad613cc0028
git <82054121+KXzeno@users.noreply.github.com>
2024-12-02 11:44:54 -0800

    FEAT: implement date field parser

--- utils/DateInserter.java
+++ utils/DateInserter.java
@@ -9,11 +9,35 @@ import java.nio.charset.StandardCharsets;
 // import java.nio.file.FileSystem;
 // import java.nio.file.StandardOpenOption;
 import java.lang.CharSequence;
-
 import java.nio.file.Path;
 
 public class DateInserter {
-  DateInserter() {
+
+  public DateInserter() {
+  }
+
+  public boolean start = false;
+  private StringBuilder matcher = new StringBuilder();
+  private int matched = 0;
+
+  private void updateMatcher(String line) {
+    String shallowLine = line;
+    // Only handles curlies for now
+    final String opening = "{";
+    final String closing = "}";
+
+    shallowLine = shallowLine.replaceAll("[^\\{|\\}]", "");
+    this.matcher.append(shallowLine);
+
+    for (int i = 0; i < this.matcher.toString().length(); i++) {
+      if (this.matcher.indexOf(opening) != -1 && this.matcher.indexOf(closing) != -1) {
+        matcher.deleteCharAt(matcher.lastIndexOf(opening));
+        matcher.deleteCharAt(matcher.indexOf(closing));
+        if (this.matcher.length() == 0) {
+          this.matched++;
+        }
+      } 
+    }
   }
 
   public void modifyDateFields(String[] args) {
@@ -23,7 +47,7 @@ public class DateInserter {
       for (String arg : args) {
         input.append(arg).append(" ");
       }
-      // TODO: Begin file search
+
       String inputPath = input.toString().trim();
       File inputFile = new File(new StringBuilder("./").append(inputPath).toString());
       Path target = inputFile.toPath();
@@ -31,13 +55,15 @@ public class DateInserter {
         try {
           BufferedReader reader = Files.newBufferedReader(target, StandardCharsets.UTF_8);
           CharSequence csq = reader.lines().map(line -> {
-            if (line.contains("C Date")) {
-              return "{Pending Creation Date}";
-            } else if (line.contains("E Date")) {
-              return "{Pending Edited Date}";
-            } else {
+            if (line.contains("\\frontmatter") || this.start == true) {
+              this.start = true;
+              int prevMatched = this.matched;
+              updateMatcher(line);
+              if (this.matched >= 5 && this.matched <= 6 && prevMatched != this.matched) {
+                System.out.println(line);
+              }
               return line;
-            }
+            } else { return line; }
           }).reduce("", (a, b) -> a + b + "\n");
 
           reader.close();
