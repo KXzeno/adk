ebb32fc16c1f960c61e27924c5f5dabe11bb74de
git <82054121+KXzeno@users.noreply.github.com>
2024-12-06 08:49:44 -0800

    refactor: extract prototype parsing logic

--- utils/adr/Enclave.java
+++ utils/adr/Enclave.java
@@ -1,14 +1,7 @@
 package utils.adr;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.util.Scanner;
-import java.nio.file.Path;
-import java.net.URI;
-
 import utils.exceptions.InvalidProtocolException;
-import utils.resolvers.*;
+import utils.PrototypeReader;
 
 public class Enclave {
   private static final String RESET = "\033[0m"; 
@@ -16,40 +9,15 @@ public class Enclave {
 
   public Enclave() {}
 
-  private static String reducer(String prev, String next) {
-    return prev + next + '\n';
-  }
-
   private static void generateBP(String[] args) {
-    // ClassLoader to access resources with respect to jar root, accessible by .class
-    ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-    // ClassPath to navigate user's FileSystem with respect to location of initialized JVM 
-    URI classPath = new File(System.getProperty("java.class.path")).toURI();
-    String targetPath = "";
     try {
-      final String PROTOCOL = URIParser.identifyScheme(classLoader);
-      switch (PROTOCOL) {
-        case "jar": targetPath = new JARPrototypeResolver().resolve(classLoader).toString(); break;
-        case "file": targetPath = new PrototypeResolver().resolve(Path.of(classPath)).toString(); break;
-        default: throw new InvalidProtocolException("Scheme is not of protocol \\'file\\' or \\'jar\\'");
-      }
-      String formattedTargetPath = targetPath.replaceAll("\\\\", "/");
-      URI targetURI = URI.create(formattedTargetPath);
+      PrototypeReader protoReader = new PrototypeReader();
       for (String arg : args) {
         System.out.println(arg);
       }
-      System.out.println(targetURI);
-
-      // try {
-      //   BufferedReader bpReader = Files.newBufferedReader(new File(targetURI).toPath());
-      //   CharSequence fileText = bpReader.lines().reduce("", Enclave::reducer);
-      //   bpReader.close();
-      //   BufferedWriter bpWriter = Files.newBufferedWriter(newFile(""))
-      // } catch (IOException x) {
-      //   System.err.println(x);
-      // }
+      System.out.println(PURPLE + protoReader.getPrototype() + RESET);
     } catch (InvalidProtocolException x) {
-      System.err.println(x);
+      System.out.println(x);
     }
   }
 
