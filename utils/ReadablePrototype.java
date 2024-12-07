a27f49939b9ab1842467251fea287dfdecb736c7
git <82054121+KXzeno@users.noreply.github.com>
2024-12-07 03:25:14 -0800

    docs: elucidate Prototype def

--- utils/ReadablePrototype.java
+++ utils/ReadablePrototype.java
@@ -8,6 +8,8 @@ import utils.exceptions.InvalidProtocolException;
  * A ReadablePrototype is a URI source that exposes itself to the caller through a
  * parameterless getPrototype method
  * 
+ * The term Prototype is used outside programming context; they are instances of a LaTeX document template.
+ *
  * @author Kx
  */
 public interface ReadablePrototype {
