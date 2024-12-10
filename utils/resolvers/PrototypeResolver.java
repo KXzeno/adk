dd8bb8d63bcfe6a830bc4697225832a73547499c
git <82054121+KXzeno@users.noreply.github.com>
2024-12-09 21:04:48 -0800

    feat: implement base path resolver

--- utils/resolvers/PrototypeResolver.java
+++ utils/resolvers/PrototypeResolver.java
@@ -12,8 +12,20 @@ public class PrototypeResolver implements Resolver {
   @Override
   public Path resolve(Path path) {
     this.binaryPath = new BinaryResolver().resolve(path);
-    this.assetsPath = Paths.get(binaryPath.toString().replace("\\bin", "\\assets"));
-    return assetsPath.resolve("boilerplate.tex");
+    this.assetsPath = Paths.get(this.binaryPath.toString().replace("\\bin", "\\assets"));
+    return this.assetsPath.resolve("boilerplate.tex");
   }
 
+  /**
+   * Returns path of the project root of which the prototype resides.
+   * @param path the path reference used to resolve to project root
+   * @return base path
+   */
+  public Path resolveToBasePath(Path path) {
+    if (this.binaryPath != null) {
+      this.binaryPath = new BinaryResolver().resolve(path);
+    }
+    this.assetsPath = Paths.get(this.binaryPath.toString().replace("\\bin", "\\"));
+    return this.assetsPath
+  }
 }
