6c884b9beded943076813ce11ffbaca565ef3c21
git <82054121+KXzeno@users.noreply.github.com>
2024-12-09 21:04:18 -0800

    feat: implement base path resolver

--- utils/resolvers/JARPrototypeResolver.java
+++ utils/resolvers/JARPrototypeResolver.java
@@ -2,6 +2,7 @@ package utils.resolvers;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
 
 public class JARPrototypeResolver {
   public JARPrototypeResolver() {}
@@ -13,6 +14,20 @@ public class JARPrototypeResolver {
   public URI resolve(ClassLoader cl) {
     try {
       return cl.getResource("assets/boilerplate.tex").toURI();
+    } catch (URISyntaxException x) {
+      System.err.println(x);
+    }
+    return null;
+  }
+
+  /**
+   * Returns path of the project root of which the prototype resides.
+   * @param path the path reference used to resolve to project root
+   * @return base URI
+   */
+  public URI resolveToBasePath(ClassLoader cl) {
+    try {
+      URI newURI = new URI(protoURI.toString().replaceAll("(^jar\\:)([\\S\\s]+)((?<=adk/)[\\S\\s]+jar!/)(?:dist/)?([\\S\\s]+)", "$2"));
     } catch (URISyntaxException x) {
       System.err.println(x);
     }
