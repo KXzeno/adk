87bac67c60ad03e87d64b7235aad71d490379670
git <82054121+KXzeno@users.noreply.github.com>
2024-12-09 19:16:09 -0800

    feat: construct `rebound` method for `Revalidator`

--- utils/resolvers/Validator.java
+++ utils/resolvers/Validator.java
@@ -1,13 +1,21 @@
 package utils.resolvers;
 
-import utils.ReadablePrototype;
-
 /**
+ * Generic validator interface that ensures the exisence of a 
+ * required source that is not contained in the JRE or runtime image, 
+ * it may also be used to ensure types at runtime.
  * @author Kx
  */
-public interface Validator<T> extends ReadablePrototype {
-/**
- * @
- */
-  boolean validate(T path);
+public interface Validator<T> {
+  /**
+   * Validates the argument if it is an existing instance or source
+   * @param T the argument to validate
+   */
+  boolean validate(T obj);
+
+  /**
+   * Undergoes data recreation of source or instance is revalidated, the
+   * extent of retrieval is left to implementation.
+   */
+  T rebound();
 }
