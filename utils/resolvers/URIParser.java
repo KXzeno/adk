9b5a3ef7af5ff4eb813459fbdac3ec97fbc7d996
git <82054121+KXzeno@users.noreply.github.com>
2024-12-10 17:29:37 -0800

    refactor: excise unchecked exceptions

--- utils/resolvers/URIParser.java
+++ utils/resolvers/URIParser.java
@@ -1,6 +1,5 @@
 package utils.resolvers;
 
-import java.lang.NullPointerException;
 import java.net.URISyntaxException;
 
 public class URIParser {
@@ -8,12 +7,14 @@ public class URIParser {
     String scheme = null;
     try {
       scheme = cl.getResource("").toURI().getScheme();
-    } catch (NullPointerException | URISyntaxException x1) {
+    } catch (URISyntaxException x1) {
       try {
         scheme = cl.getResource("META-INF").toURI().getScheme();
-      } catch (NullPointerException | URISyntaxException x2) {
+      } catch (URISyntaxException x2) {
         System.err.println(x1);
+        x1.printStackTrace();
         System.err.println(x2);
+        x2.printStackTrace();
       }
     }
     return scheme;
